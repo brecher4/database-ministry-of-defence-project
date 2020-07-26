fc5cf40dd744fd5524e31bfed06afd943d147fa3
brecher4 <bt@sem-wolf>
2020-07-27 02:49:55 +0300

    add insert_record and get_record functions

--- SRC/db.py
+++ SRC/db.py
@@ -1,6 +1,6 @@
-import shelve, os
-from db_api import DB, DBTable, DBField, SelectionCriteria
+import os, shelve
 from typing import Any, Dict, List, Type
+from db_api import DB, DBTable, DBField, SelectionCriteria
 
 
 path_root = 'db_files'
@@ -22,8 +22,7 @@ class DataBaseTable(DBTable):
         self.path_file = os.path.join(path_root, self.name + '.db')
 
         # create shelve file
-        path = os.path.join('db_files', name + '.db')
-        s = shelve.open(path)
+        s = shelve.open(self.path_file)
         s.close()
 
 
@@ -32,27 +31,43 @@ class DataBaseTable(DBTable):
 
 
     def insert_record(self, values: Dict[str, Any]) -> None:
-        raise NotImplementedError
+        if self.key_field_name not in values.keys():
+            raise KeyError("The key is missing")
+
+        if not set(values.keys()).issubset(set(self.fields)):
+            raise KeyError("There are fields that not exists in the table's fields")
+
+        s = shelve.open(self.path_file)
+        s[values[self.key_field_name]] = values
+        s.close()
+        
 
     def delete_record(self, key: Any) -> None:
         raise NotImplementedError
 
+
     def delete_records(self, criteria: List[SelectionCriteria]) -> None:
         raise NotImplementedError
 
+
     def get_record(self, key: Any) -> Dict[str, Any]:
-        raise NotImplementedError
+        s = shelve.open(self.path_file)
+        record = s.get(key, None)
+        s.close()
+        return record
+
 
     def update_record(self, key: Any, values: Dict[str, Any]) -> None:
         raise NotImplementedError
+
 
     def query_table(self, criteria: List[SelectionCriteria]) \
             -> List[Dict[str, Any]]:
         raise NotImplementedError
 
+
     def create_index(self, field_to_index: str) -> None:
         raise NotImplementedError
-
 
 
 class DataBase(DB):
@@ -61,7 +76,7 @@ class DataBase(DB):
         self.db_tables = {}
         self.num_tables = 0
 
-    
+
     def create_table(self, table_name: str, fields: List[DBField], key_field_name: str) -> DBTable:
         self.db_tables[table_name] = DataBaseTable(table_name, fields, key_field_name)
         self.num_tables += 1
